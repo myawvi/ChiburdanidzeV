1190c9ca4efd6d85cbec22a3bb7b2f0ef5e92bd8
myawvi <lera2006200650@gmail.com>
2024-10-03 10:15:59 +0300

    Add files via upload

---
+++ задание 2.py
@@ -0,0 +1,8 @@
+from math import *
+x = 14.26
+y = -1.22
+z = 3.5 * 10**(-2)
+
+s = ( (2 * cos(x - (2/3)) ) / ((1/2) + pow((sin(y)), 2)) ) * (1 + ( (pow(z, 2)) / (3 - pow(z, 2) / 5) ))
+
+print(round(s,2))
